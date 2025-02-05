ac663b9439c69bd377eb291a941b7ddd84038eff
changhyun <19941128o@gmail.com>
2025-02-05 18:57:59 +0900

    commit

--- nestks-board-app/src/boards/boards.controller.ts
+++ nestks-board-app/src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -12,8 +13,23 @@ export class BoardsController {
     }
 
     @Post()
-    createBoard(@Body('title') title: string, @Body('description') description: string) : Board {
-        return this.boardsService.createBoard(title, description);
+    createBoard(@Body() createBoardDto : CreateBoardDto) : Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Get('/:id')
+    getBoardById(@Param('id') id: string) : Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string) : void {
+        return this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus) {
+        return this.boardsService.updateBoardStatus(id, status);
     }
 
 }
--- nestks-board-app/src/boards/boards.service.ts
+++ nestks-board-app/src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v4 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -10,7 +11,9 @@ export class BoardsService {
         return this.boards;
     }
 
-    createBoard(title: string, description: string) : Board {
+    createBoard(createBoardDto : CreateBoardDto) : Board {
+        const {title, description} = createBoardDto;
+
         const board: Board = {
             id: uuid(),
             title,
@@ -22,11 +25,24 @@ export class BoardsService {
         return board;
     }
 
-    getBoardById(id: string) : Board | undefined{
+    getBoardById(id: string) : Board{
         const found = this.boards.find(board => board.id === id);
         if(!found){
             throw new NotFoundException(`Board with ID "${id}" not found`);
         }
         return found;
+    }
+
+    deleteBoard(id : string) : void {
+        this.boards = this.boards.filter(board => board.id !== id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus) : Board {
+        const board = this.getBoardById(id);
+        if (!board) {
+            throw new NotFoundException(`Board with ID "${id}" not found`);
+        }
+        board.status = status; 
+        return board;
     }
 }
